76a2b55f44357d24c79cd4b8415e12e9f0b8826b
ivajunu <125510283+ivajunu@users.noreply.github.com>
2023-02-15 23:22:13 +0100

    Add files via upload
    Projekt labb 2

---
+++ CatFact.vue
@@ -0,0 +1,49 @@
+<script>
+
+import axios from 'axios'
+
+
+
+export default {
+  data() {
+   return {fact: null }
+  },
+  created() {
+    axios
+    .get('https://cat-fact.herokuapp.com/facts')
+    .then((fact) => {
+      console.log(fact.data)
+      this.fact = fact.data
+    })
+
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <h3>Catfacts:</h3>
+    <ul>
+      <li :key="facts.id" v-for="facts in fact">{{ facts.text }}</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+h3, li {
+  font-size: 20px;
+}
+h3 {
+  text-decoration: underline;
+}
+li {
+  list-style-type: circle;
+  padding: 5px;
+}
+div {
+  margin-left: 30px;
+}
+
+</style>
